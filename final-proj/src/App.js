feb4e46631fd05c95dd111e635e6c903848bef77
YuraOvcharov <yuraovcharov13@gmail.com>
2019-09-18 06:28:36 +0300

    create navigation

--- final-proj/src/App.js
+++ final-proj/src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import Navigation from './components/Navigation/Navigation';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//You must add your own API key here from Clarifai.
+const app = new Clarifai.App({
+  apiKey: 'YOUR_API_HERE'
+});
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isSignedIn: false,
+    }
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({ isSignedIn: false })
+    } else if (route === 'home') {
+      this.setState({ isSignedIn: true })
+    }
+    this.setState({ route: route });
+  }
+
+  render() {
+    const { isSignedIn } = this.state;
+    return (
+      <div className="App">
+        <Particles className='particles' params={particlesOptions} />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+
+      </div>
+    );
+  }
 }
 
 export default App;
