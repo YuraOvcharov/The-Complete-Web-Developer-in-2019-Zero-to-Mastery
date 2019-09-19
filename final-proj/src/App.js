00ec0c55fa000be597603b6d8aa8f9f26b51f539
YuraOvcharov <yuraovcharov13@gmail.com>
2019-09-19 06:23:16 +0300

    add packeges

--- final-proj/src/App.js
+++ final-proj/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
+// import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 
 //You must add your own API key here from Clarifai.
