a29c6ee025474be9523edb0ff6ad823b6f9732f4
YuraOvcharov <yuraovcharov13@gmail.com>
2019-09-19 06:48:16 +0300

    add FaceRecognition

--- final-proj/src/App.js
+++ final-proj/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
-// import Clarifai from 'clarifai';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 
 //You must add your own API key here from Clarifai.
-// const app = new Clarifai.App({
-//   apiKey: 'YOUR_API_HERE'
-// });
+const app = new Clarifai.App({
+  apiKey: '64d4d6e422544b71a09d2844c5db2a9f'
+});
 
 const particlesOptions = {
   particles: {
@@ -37,13 +40,49 @@ class App extends Component {
     this.setState({ route: route });
   }
 
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  }
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count }))
+            })
+
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
     const { isSignedIn } = this.state;
     return (
       <div className="App">
         <Particles className='particles' params={particlesOptions} />
         <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
-
+        <Logo />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition />
       </div>
     );
   }
