aeb506e85f0ed6c90362f538f2744b83aa159598
YuraOvcharov <yuraovcharov13@gmail.com>
2019-09-19 07:05:59 +0300

    add Rank componet

--- final-proj/src/App.js
+++ final-proj/src/App.js
@@ -42,22 +42,29 @@ class App extends Component {
     }
   }
 
-  onRouteChange = (route) => {
-    if (route === 'signout') {
-      this.setState({ isSignedIn: false })
-    } else if (route === 'home') {
-      this.setState({ isSignedIn: true })
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
     }
-    this.setState({ route: route });
   }
 
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
 
   onInputChange = (event) => {
-    this.setState({ input: event.target.value });
+    this.setState({input: event.target.value});
   }
 
   onButtonSubmit = () => {
-    this.setState({ imageUrl: this.state.input });
+    this.setState({imageUrl: this.state.input});
     app.models
       .predict(
         Clarifai.FACE_DETECT_MODEL,
@@ -66,14 +73,14 @@ class App extends Component {
         if (response) {
           fetch('http://localhost:3000/image', {
             method: 'put',
-            headers: { 'Content-Type': 'application/json' },
+            headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
               id: this.state.user.id
             })
           })
             .then(response => response.json())
             .then(count => {
-              this.setState(Object.assign(this.state.user, { entries: count }))
+              this.setState(Object.assign(this.state.user, { entries: count}))
             })
 
         }
